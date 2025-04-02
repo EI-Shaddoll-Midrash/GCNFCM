fa14c5ff441c33f86046f3cd8ad4213eb5b43684
EI-Shaddoll-Midrash <145334353+EI-Shaddoll-Midrash@users.noreply.github.com>
2025-04-02 16:09:55 +0800

    Update GCNFCM.py

--- code/GCNFCM.py
+++ code/GCNFCM.py
@@ -1,3 +1,5 @@
+import time
+
 from torch_geometric.utils import to_dense_adj
 import FCMQ
 import GCNAE
@@ -43,22 +45,26 @@ def generate_features(edge_index):
     return features
 
 
-def GCNFCM(data,num):
+def GCNFCM(data, num):
+    t1 = time.time()
     # dataset = PolBlogs(root='./data/Polblogs')
     # data = dataset[0]
-    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # 使用新方法生成特征矩阵
     A = to_dense_adj(data.edge_index).squeeze()
     A = A.float()
+    # 无特征矩阵的数据集
     X = generate_features(data.edge_index)
+    # 有特征矩阵的数据集
+    # X = data.x
     
     # 预处理步骤
     A_norm = GCNAE.pre_process(A)
     
     # 初始化GCNAE模型 此为小模型使用
-    #encoder = GCNAE.GCNEncoder(input_dim=X.size(1), hidden_dim=256, output_dim=32)
+    encoder = GCNAE.GCNEncoder(input_dim=X.size(1), hidden_dim=256, output_dim=32)
     # 初始化GCNAE模型 此为中模型使用
-    encoder = GCNAE.GCNEncoder(input_dim=X.size(1), hidden_dim1=256,hidden_dim2=128,output_dim=32)
+    # encoder = GCNAE.GCNEncoder(input_dim=X.size(1), hidden_dim1=256, hidden_dim2=128, output_dim=32).to(device)
     optimizer = torch.optim.Adam(encoder.parameters(), lr=0.01)
     lambda_param = 0.1
     # 训练GCNAE模型
@@ -68,42 +74,50 @@ def GCNFCM(data,num):
         ipd_decoder = GCNAE.IPDDecoder(latent_dim=32)
         A_hat = ipd_decoder(z)
         # 此为小模型使用
-        # gcd_decoder = GCNAE.GCDDecoder(input_dim=32, hidden_dim=256, output_dim=X.size(1))
+        gcd_decoder = GCNAE.GCDDecoder(input_dim=32, hidden_dim=256, output_dim=X.size(1))
         # 此为中模型使用
-        gcd_decoder = GCNAE.GCDDecoder(input_dim=32, hidden_dim1=128, hidden_dim2=256,output_dim=X.size(1))
+        # gcd_decoder = GCNAE.GCDDecoder(input_dim=32, hidden_dim1=128, hidden_dim2=256, output_dim=X.size(1))
         X_hat = gcd_decoder(z, A_norm)
         loss = GCNAE.total_loss(A, A_hat, X, X_hat, lambda_param)
-        # L = loss
         loss.backward()
         optimizer.step()
         # 动态调整 lambda_param
-        if epoch % 10 == 0:
-            L1 = GCNAE.L1_loss(A, A_hat)
-            L2 = GCNAE.L2_loss(X, X_hat)
-            if L1 > L2:
-                lambda_param *= 1.05  # 增加 L1 的权重
-            else:
-                lambda_param *= 0.95  # 减少 L1 的权重
-        
+        # if epoch % 10 == 0:
+        #     L1 = GCNAE.L1_loss(A, A_hat)
+        #     L2 = GCNAE.L2_loss(X, X_hat)
+        #     if L1 > L2:
+        #         lambda_param *= 1.05  # 增加 L1 的权重
+        #     else:
+        #         lambda_param *= 0.95  # 减少 L1 的权重
+        # L = loss
         if (epoch + 1) % 10 == 0:
             print(f'Epoch [{epoch + 1}/{num}], Loss: {loss.item():.4f}, lambda_param: {lambda_param:.4f}')
     
     # 使用GCNAE的潜在表示进行FCMQ社区检测
     Z = z.detach().numpy()
+    # np.save('acm_embeddings.npy', Z)
+    # print("ACM数据集的嵌入结果已保存到 'acm_embeddings.npy' 文件中。")
+    # # 保存为文本文件
+    # np.savetxt('acm_embeddings.txt', Z, fmt='%.6f', delimiter=' ')
+    # print("Embeddings saved to 'acm_embeddings.txt'")
     WE = np.ones(A.shape)  # 假设所有边的权重为1
     U_best, Q_hat = FCMQ.fcmq(Z, A, WE, epsilon=0.001, max_c=10, m=2, lamb=0.01)
-    
+    t2 = time.time()
+    print(f"算法共用时:{t2 - t1:.2f}s")
     # print("Best Soft Assignment Matrix U_best:", U_best)
     threshold = 0.5
     y_pred = (U_best > threshold).astype(int).argmax(axis=1)
     
     # 真实的标签
     y_true = data.y.numpy().squeeze()
+    # 检查 y_true 的形状
+    if y_true.shape == ():  # 如果是标量
+        y_true = np.array([y_true])  # 转换为一维数组
     if y_true.ndim != 1:
         raise ValueError(f"y_true must be a 1D array, but got shape {y_true.shape}")
     # 计算 NMI，ARI，Q_hat
-    evaluate.nmi(y_true, y_pred)
-    evaluate.ari(y_true, y_pred)
+    # evaluate.nmi(y_true, y_pred)
+    # evaluate.ari(y_true, y_pred)
     print(f'Modularity Computation(˜ Q): {Q_hat:.4f}')
     # 计算 Omega 指标
     # 将 y_true 和 y_pred 转换为社区划分的字典格式
@@ -112,22 +126,19 @@ def GCNFCM(data,num):
         if comm not in comms1:
             comms1[comm] = []
         comms1[comm].append(node)
-    
     comms2 = {}
     for node, comm in enumerate(y_pred):
         if comm not in comms2:
             comms2[comm] = []
         comms2[comm].append(node)
-    
-    omega = Omega(comms1, comms2)
-    print(f'Omega Score: {omega.omega_score:.4f}')
-    
     # 计算 ONMI 指标
     cover_true = [set(comm) for comm in comms1.values()]
     cover_pred = [set(comm) for comm in comms2.values()]
     all_nodes = set(range(len(y_true)))
     onmi_score = onmi(cover_pred, cover_true, all_nodes, variant="LFK")
     print(f'ONMI Score: {onmi_score:.4f}')
+    omega = Omega(comms1, comms2)
+    print(f'Omega Score: {omega.omega_score:.4f}')
     
     # 计算 EQ 指标
     # 将 y_pred 转换为社区划分的格式
